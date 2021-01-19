4f7d373cc2346808b2c4454880f4084b007fa46a
YunSeoWon <floidea@naver.com>
2021-01-20 01:08:43 +0900

    도커 및 쉘스크립트 설정

---
+++ service-down.sh
@@ -0,0 +1,8 @@
+docker stop account-service;
+docker rm account-service;
+docker stop order-service;
+docker rm order-service;
+docker stop product-service;
+docker rm product-service;
+docker stop transaction-service;
+docker rm transaction-service;
